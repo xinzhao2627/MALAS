895f8bde593a8989611d5097353169f4c3573725
xinzhao2627 <94624435+xinzhao2627@users.noreply.github.com>
2024-05-11 11:50:50 +0800

    Montaniel submain

--- malas/src/App.js
+++ malas/src/App.js
@@ -1,18 +1,6 @@
 import React from 'react';
 import './App.css';
-
-class Login extends React.Component{
-  render (){
-
-    return (
-      <div className='login-section '>
-        <header className="App-header">
-          Microsoft Alternative Login Authentication System
-        </header>
-      </div>
-    )
-  }
-}
+import Login from './Login';
 
 function App() {
   return (
---
+++ malas/src/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+class Login extends React.Component{
+    render (){
+  
+      return (
+        <div className='login-section '>
+          <header className="App-header">
+            Microsoft Alternative Login Authentication System hai
+          </header>
+        </div>
+      )
+    }
+}
+
+export default Login
