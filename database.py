2372e2c18d50d239bcb0625af34d7e3a9c646a2e
Carl <carlarvinhipolito14@gmail.com>
2024-05-16 17:46:10 +0800

    create method - hipolito

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import mysql.connector
 
+#def connectSQL():
 cnx = mysql.connector.connect(
     host="localhost",
     user = "root",
@@ -7,18 +8,15 @@ cnx = mysql.connector.connect(
     database = "microsoft_database"
 )
 
-cursor = cnx.cursor()
-
 #CREATE METHODS
 
 def insert_data_to_user(user_id, username, user_password, f_name, l_name, user_contact_num, user_email, user_birthdate, user_country):
+    cursor = cnx.cursor()
     query = "INSERT INTO user(user_id, username, user_password, f_name, l_name, user_contact_num, user_email, user_birthdate, user_country) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
     data = (user_id, username, user_password, f_name, l_name, user_contact_num, user_email, user_birthdate, user_country)
     cursor.execute(query, data)
     cnx.commit()
     cursor.close()
-    cnx.close()
-
 
 
 #READ METHODS
@@ -32,4 +30,6 @@ def insert_data_to_user(user_id, username, user_password, f_name, l_name, user_c
 
 #EXECUTE/CALL METHODS
 
-insert_data_to_user(150003002, "Shii", "password1234", "Carl", "Hipolito" ,"09123456789", "sample@domain.com", "2003-09-14", "Philippines")
+insert_data_to_user(150003001, "Username1", "password1", "Carl", "Hipolito" ,"09123456789", "sample1@domain.com", "2003-09-14", "Philippines")
+insert_data_to_user(150003002, "Username2", "password2", "Neil", "Baltar" ,"09123456788", "sample2@domain.com", "2003-01-01", "Philippines")
+insert_data_to_user(150003003, "Username3", "password3", "Rainnand", "Montaniel" ,"09123456787", "sample3@domain.com", "2003-01-02", "Philippines")
