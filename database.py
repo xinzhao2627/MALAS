9cb05869820c02f03f976685807c8782ddb057a8
Carl <carlarvinhipolito14@gmail.com>
2024-05-16 17:33:09 +0800

    added insert method in user table - hipolito

--- database.py
+++ database.py
@@ -10,15 +10,26 @@ cnx = mysql.connector.connect(
 cursor = cnx.cursor()
 
 #CREATE METHODS
-query = "INSERT INTO user(user_id, username, user_password, f_name, l_name, user_contact_num, user_email, user_birthdate, user_country) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-data = (150003001, "Shii", "password1234", "Carl", "Hipolito" ,"09123456789", "sample@domain.com", "2003-09-14", "Philippines")
-cursor.execute(query, data)
-cnx.commit()
-cursor.close()
-cnx.close()
+
+def insert_data_to_user(user_id, username, user_password, f_name, l_name, user_contact_num, user_email, user_birthdate, user_country):
+    query = "INSERT INTO user(user_id, username, user_password, f_name, l_name, user_contact_num, user_email, user_birthdate, user_country) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    data = (user_id, username, user_password, f_name, l_name, user_contact_num, user_email, user_birthdate, user_country)
+    cursor.execute(query, data)
+    cnx.commit()
+    cursor.close()
+    cnx.close()
+
+
 
 #READ METHODS
 
+
 #UPDATE METHODS
 
+
 #DELETE METHODS
+
+
+#EXECUTE/CALL METHODS
+
+insert_data_to_user(150003002, "Shii", "password1234", "Carl", "Hipolito" ,"09123456789", "sample@domain.com", "2003-09-14", "Philippines")
